96dce1bd241c85ec983fdcc0a02346b9578fcba4
jorgeruedasanchez <jorgerueda@ugr.es>
2025-07-24 00:17:55 +0200

    Add files via upload

---
+++ Simulation Study 1/main.R
@@ -0,0 +1,73 @@
+library(MASS)
+library(tidyverse)
+library(mlbench)
+library(caret)
+library(survey)
+library(sampling)
+library(NonProbEst)
+library(xgboost)
+library(dplyr)
+library(foreign)
+library(RcmdrMisc)
+library(samplingVarEst)
+# install.packages("devtools")
+# library(devtools)
+library(fastDummies)
+library(parallel)
+library(lme4)
+#library(FSelector)
+#remotes::install_github("ncn-foreigners/nonprobsvy")
+library(nonprobsvy)
+library(Frames2)
+#library(xlsx)
+# if (!require("devtools")) install.packages("devtools")
+# devtools::install_github("chkern/KWML")
+library(KWML)
+#install.packages("nnet")
+library(nnet)
+library(xlsx)
+
+cluster = makeCluster(3) #para que no coja todos los nucleos? OJO QUE LOS COJO TODOS PARA QUE TARDE MENOS
+clusterEvalQ(cluster,library(MASS))
+clusterEvalQ(cluster,library(tidyverse))
+clusterEvalQ(cluster,library(mlbench))
+clusterEvalQ(cluster,library(xgboost))
+clusterEvalQ(cluster,library(foreign))
+clusterEvalQ(cluster,library(RcmdrMisc))
+clusterEvalQ(cluster,library(fastDummies))
+clusterEvalQ(cluster,library(lme4))
+clusterEvalQ(cluster,library(NonProbEst))
+clusterEvalQ(cluster,library(caret))
+clusterEvalQ(cluster,library(sampling))
+clusterEvalQ(cluster,library(survey))
+#clusterEvalQ(cluster,library(FSelector))
+clusterEvalQ(cluster,library(KWML))
+clusterEvalQ(cluster,library(samplingVarEst))
+#remotes::install_github("ncn-foreigners/nonprobsvy")
+clusterEvalQ(cluster,library(nonprobsvy))
+clusterEvalQ(cluster,library(dplyr))
+clusterEvalQ(cluster,library(Frames2))
+#clusterEvalQ(cluster,library(xlsx))
+clusterEvalQ(cluster,library(KWML))
+clusterEvalQ(cluster,library(nnet))
+
+## var
+n_r=1000
+source("simulation var.R")  
+setwd("...")
+write.xlsx(results, paste0("results_var",n_r,".xlsx"))
+
+## nr
+for(n_r in c(500, 1000, 1500, 2000, 2500)){
+  source("simulation nr.R")  
+  setwd("...")
+  write.xlsx(results, paste0("results_",n_r,".xlsx"))
+}
+
+##PSA
+n_r=1000
+source("simulation PSA.R")  
+setwd("...")
+write.xlsx(results, paste0("results_PSA",n_r,".xlsx"))
+
+stopCluster()
